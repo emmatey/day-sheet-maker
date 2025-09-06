db6d77663a187dcb6e967c670c80d446b1aa6734
Emma Talley <mcrib40@gmail.com>
2025-09-06 14:36:58 -0400

    final attempt to get this to run on the work PCs

--- backend/src/DefaultSettings.py
+++ backend/src/DefaultSettings.py
@@ -66,7 +66,7 @@ default_settings = {
     "ROLE_MAP": {
         "Blacklists": {
             "departments": ["Authorized Hours RX", "Authorized Hrs", "Default", "Light Duty", "Management", "Pre-Opening", "Storm Loss", "Support Employment", "Training"],
-            "roles": ["Asst ARM", "Scan File"]
+            "roles": ["Asst ARM", "Scan File", "Default"]
             },
         "Hannaford to Go": {
             "roles": ["Expeditor", "Shopper"],
