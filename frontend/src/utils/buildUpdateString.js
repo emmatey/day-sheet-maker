598e45206dec9c9e7d586ef1c58454a5d74f3601
emmatey <mcrib40@gmail.com>
2025-10-27 11:44:14 -0400

    change buildUpdateString to named export

--- frontend/src/utils/buildUpdateString.js
+++ frontend/src/utils/buildUpdateString.js
@@ -1,4 +1,4 @@
-export default function buildUpdateString(segments, value, action = "update") {
+export function buildUpdateString(segments, value, action = "update") {
   const path = "[" + segments.map(String).join("][") + "]";
   const payload = JSON.stringify(value);
   return `${path}^${payload}^${action}`;
